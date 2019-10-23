a2fd111fdbfd2b27d83bafb02c151dce8b715207
Godfrey Fernandes <godfrey.f@ET-C02WK5MEG8WN.local>
2019-10-23 23:57:39 +0530

    Added implementation of firebase in the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { AuthenticationService } from './services/authentication.service';
 
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { LanguageService } from './services/language.service';
+
+// import { FcmService } from './services/fcm.service';
+import { FCM } from '@ionic-native/fcm/ngx';
+
 
 @Component({
   selector: 'app-root',
@@ -19,16 +23,50 @@ export class AppComponent {
     private statusBar: StatusBar,
     private languageService: LanguageService,
     private authenticationService: AuthenticationService,
-    private router: Router
+    private router: Router,
+    // private fcm: FcmService,
+    public toastController: ToastController,
+    public fcm: FCM
   ) {
 
     this.initializeApp();
   }
 
+  private async presentToast(message) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  private notificationSetup() {
+    
+    // this.fcm.getToken();
+    this.fcm.onNotification().subscribe(
+      (msg) => {
+        if (this.platform.is('ios')) {
+          this.presentToast(msg.aps.alert);
+        } else {
+          this.presentToast(msg.body);
+        }
+      });
+
+    this.fcm.getToken().then(token => {
+      // Your best bet is to here store the token on the user's profile on the
+      // Firebase database, so that when you want to send notifications to this
+      // specific user you can do it from Cloud Functions.
+      console.log('----token----');
+      console.log(token)
+    });
+  }
+
+
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.notificationSetup();
 
       this.languageService.setInitialAppLanguage();
 
